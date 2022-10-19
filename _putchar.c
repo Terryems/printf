713f51dc6dd45570958c3b946ba2d3dcbff592b5
Terryems <emmanuelsimona@gmail.com>
2022-10-18 20:45:29 -0700

    printf

--- _putchar.c
+++ _putchar.c
@@ -1,66 +1,38 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return:( str-a)
  */
-int _printf(const char *format, ...)
+int _puts(char *str)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	char *a = str;/*declaration of variables*/
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1.
+ * On error, -1 is returned, and error is set.
  */
-void print_buffer(char buffer[], int *buff_ind)
+int _putchar(int c)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
 
-	*buff_ind = 0;
-}}
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
